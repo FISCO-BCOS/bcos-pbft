06e0a1e5efdf612e2cfd46db9c44c8d470a558f2
cyjseagull <yujiechen_hust@163.com>
2021-04-08 10:14:36 +0800

    Squashed 'bcos-cmake-scripts/' content from commit c0950ab
    git-subtree-dir: bcos-cmake-scripts
    git-subtree-split: c0950ab36daff9c591f283b545383c10b6472a4d

---
+++ InstallBcosFrameworkDependencies.cmake
@@ -0,0 +1,3 @@
+hunter_add_package(bcos-framework)
+find_package(bcos-framework CONFIG REQUIRED)
+find_package(Boost CONFIG REQUIRED log program_options chrono system filesystem iostreams thread)
---
+++ README.md
@@ -0,0 +1,2 @@
+# bcos-cmake-scripts
+the core common cmake files for all repos of FISCO BCOS 3.0
