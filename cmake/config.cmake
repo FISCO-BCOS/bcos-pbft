a7d0a35a8197b4b8335a4fc7367b7902cc17b96e
cyjseagull <yujiechen_hust@163.com>
2021-06-23 19:11:36 +0800

    update bcos-framework url

--- cmake/config.cmake
+++ cmake/config.cmake
@@ -1,4 +1,4 @@
 hunter_config(bcos-framework VERSION 3.0.0-local
-    URL "https://${URL_BASE}/cyjseagull/bcos-framework/archive/3ca806a57ba201e6e5702d2c9a64e3adfcde65ad.tar.gz"
-    SHA1 616b217fe69a89cfb13b04586034ff1e645d6f5e
+    URL "https://${URL_BASE}/FISCO-BCOS/bcos-framework/archive/75f19d3b9a745c2154d457dfe2bb3350ec606276.tar.gz"
+    SHA1 9645f0c8f044dac18941a4a948193a61792878d9
 )
