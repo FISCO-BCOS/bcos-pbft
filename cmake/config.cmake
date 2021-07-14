c7a09b8329e9bb2e6e6767555273e82ec30f2351
cyjseagull <yujiechen_hust@163.com>
2021-07-14 13:52:26 +0800

    notify to resetTxPool when first setup

--- cmake/config.cmake
+++ cmake/config.cmake
@@ -1,4 +1,4 @@
 hunter_config(bcos-framework VERSION 3.0.0-local
-    URL "https://${URL_BASE}/FISCO-BCOS/bcos-framework/archive/d5b633f581b502511adaae8c7940ee152b9103c4.tar.gz"
-    SHA1 cde41d7fbe00a0cba307b236a64c27e26dabee1f
+    URL "https://${URL_BASE}/FISCO-BCOS/bcos-framework/archive/90fe8bdd6d16a9218ef4f52378d615be73a378ed.tar.gz"
+    SHA1 7847579a1dc09b7902079d0ff65894302fd120cd
 )
