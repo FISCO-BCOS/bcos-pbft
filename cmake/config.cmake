d1e556a28deb49247c2b135e42c045124971d8d0
cyjseagull <1547158861@qq.com>
2021-08-29 19:07:50 +0800

    reset the sealer when the consensus_leader_period changed (#77)
    * upgrade bcos-framework to latest
    
    * reset the sealer when the consensus_leader_period changed

--- cmake/config.cmake
+++ cmake/config.cmake
@@ -1,5 +1,11 @@
 hunter_config(bcos-framework VERSION 3.0.0-local
-    URL https://${URL_BASE}/FISCO-BCOS/bcos-framework/archive/0ce681f43098422df47d5371dcd8e29658caf538.tar.gz
-    SHA1 31f98dd44a9bcc08d952edddd89460179edcfbd4
+    URL https://${URL_BASE}/FISCO-BCOS/bcos-framework/archive/c246dfe61315a91371e77097d8411c418fe82e2d.tar.gz
+    SHA1 664314efdc3589d7eec843664c342f3863fad52e
     CMAKE_ARGS HUNTER_PACKAGE_LOG_BUILD=ON HUNTER_PACKAGE_LOG_INSTALL=ON #DEBUG=ON
+)
+
+hunter_config(wedpr-crypto VERSION 1.1.0-10f314de
+	URL https://${URL_BASE}/WeBankBlockchain/WeDPR-Lab-Crypto/archive/10f314de45ec31ce9e330922b522ce173662ed33.tar.gz
+	SHA1 626df59f87ea2c6bb5128f7d104588179809910b
+	CMAKE_ARGS HUNTER_PACKAGE_LOG_BUILD=OFF HUNTER_PACKAGE_LOG_INSTALL=ON
 )
