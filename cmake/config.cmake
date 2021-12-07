134e7ce2bf0ca96cd5a3d7c6d123b264dfa83fb5
morebtcg <651932351@qq.com>
2021-12-07 15:15:01 +0800

    Update block header encode

--- cmake/config.cmake
+++ cmake/config.cmake
@@ -1,5 +1,5 @@
 hunter_config(bcos-framework VERSION 3.0.0-local
-	URL https://${URL_BASE}/FISCO-BCOS/bcos-framework/archive/80345086f18586ef1e6122d7bf5eb9688e990472.tar.gz
-	SHA1 1fabfc8bb45d3793f5a4789a01f32fb35ec691e2
+	URL https://${URL_BASE}/FISCO-BCOS/bcos-framework/archive/1003d35925569d25c84483aef4402556e6c74f18.tar.gz
+	SHA1 502eb233c2ce6f7c0e7e70b8bf64e50d25545c31
 	CMAKE_ARGS HUNTER_PACKAGE_LOG_BUILD=ON HUNTER_PACKAGE_LOG_INSTALL=ON
 )
